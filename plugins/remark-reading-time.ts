e68a160e26ceb8514b6d108782909490299e2005
kim <1347408207@qq.com>
2025-02-13 23:33:16 +0800

    feat: add i18n

--- plugins/remark-reading-time.ts
+++ plugins/remark-reading-time.ts
@@ -8,7 +8,9 @@ export function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
-    data.astro.frontmatter.minutesRead =
-      Math.round(readingTime.minutes) + ' Min Read';
+    // data.astro.frontmatter.minutesRead = i18next.t('blog.readingTime', {
+    //   minutes: Math.round(readingTime.minutes),
+    // });
+    data.astro.frontmatter.minutesRead = Math.round(readingTime.minutes);
   };
 }
---
+++ src/i18n/lang/en-us.ts
@@ -0,0 +1,16 @@
+export default {
+  /** blog page */
+  blog: {
+    /** last modified */
+    lastModified: 'Last edited',
+    /** reading time */
+    readingTime: '{{minutes}} Min Read'
+  },
+  /** 404 page */
+  404: {
+    /** page text */
+    pageText: 'Page Not Found',
+    /** back button text */
+    backBtnText: 'Back to Home'
+  }
+}
